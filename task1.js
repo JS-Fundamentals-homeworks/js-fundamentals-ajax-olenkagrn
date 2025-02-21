f6ad01d95bbb4a17a41a82f3bb1e1f79879d1522
unknown <olena.hrynko@gmail.com>
2025-02-21 13:01:27 +0200

    finished fetch tasks

--- task1.js
+++ task1.js
@@ -1,7 +1,22 @@
-// Завдання: отримання даних про користувачів 
+// Завдання: отримання даних про користувачів
 // За допомогою засобі Fetch отримати інформацію про користувачів
-// за посиланням - https://jsonplaceholder.typicode.com/users 
+// за посиланням - https://jsonplaceholder.typicode.com/users
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
-// Перевірити за допомогою команди npm tests/task1.test.js 
+// Перевірити за допомогою команди npm tests/task1.test.js
+
+const ul = document.querySelector(".usersList");
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) =>
+    response.json().then((users) => {
+      users.forEach((user) => {
+        const li = document.createElement("li");
+        li.textContent = user.name;
+        ul.appendChild(li);
+      });
+    })
+  )
+  .catch((error) => {
+    console.error("Error fetching users:", error);
+  });
--- task2.js
+++ task2.js
@@ -5,3 +5,29 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const userInput = document.getElementById("userNameInput");
+const sendBtn = document.getElementById("getUserButton");
+const userCity = document.getElementById("userCity");
+
+sendBtn.addEventListener("click", () => {
+  const userName = userInput.value.toLowerCase();
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => {
+      const foundUser = users.find(
+        (user) => user.name.toLowerCase() === userName
+      );
+
+      if (foundUser) {
+        userCity.textContent = foundUser.address.city;
+      } else {
+        userCity.textContent = "Користувача не знайдено.";
+      }
+    })
+    .catch((error) => {
+      console.error("Помилка отримання даних:", error);
+      userCity.textContent = "Помилка отримання даних.";
+    });
+});
